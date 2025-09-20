ad3b6ff222998f64da3a98f12902bd39d00bea47
paulo.cavalcanti <paulo.cavalcanti@wildlifestudios.com>
2025-09-20 11:26:25 -0300

    feat: generate and save graph

--- app/graph.py
+++ app/graph.py
@@ -50,3 +50,11 @@ def build_graph():
     
     app = workflow.compile()
     return app
+
+if __name__ == "__main__":
+    graph = build_graph()
+    graph_bytes = graph.get_graph().draw_mermaid_png()
+    with open("docs/graph.png", "wb") as f:
+        f.write(graph_bytes)
+    print("Grafo salvo em docs/graph.png")
+        
